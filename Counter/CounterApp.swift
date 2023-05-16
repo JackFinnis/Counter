7d9a860b9b9fe5181886160033f47460b8917baa
Jack Finnis <jack.finnis@icloud.com>
2023-05-16 17:48:40 +0100

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Jack Finnis on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
