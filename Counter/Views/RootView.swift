3c5ad32ae0c23b6fac9cb8fe86ef2e027bb783dc
Jack Finnis <jack.finnis@icloud.com>
2023-05-18 02:03:41 +0100

    Tweaks

--- Counter/Views/RootView.swift
+++ Counter/Views/RootView.swift
@@ -10,7 +10,6 @@ import MessageUI
 
 struct RootView: View {
     @AppStorage("count") var count = 0
-    @State var showResetAlert = false
     @State var showShareSheet = false
     @State var showEmailSheet = false
     
@@ -31,75 +30,65 @@ struct RootView: View {
             .onTapGesture(count: 1) {
                 count += 1
             }
-            .overlay(alignment: .topLeading) {
-                Menu {
-                    Button {
-                        showShareSheet.toggle()
-                    } label: {
-                        Label("Share \(Constants.name)", systemImage: "square.and.arrow.up")
-                    }
-                    Button {
-                        Store.requestRating()
-                    } label: {
-                        Label("Rate \(Constants.name)", systemImage: "star")
-                    }
-                    Button {
-                        Store.writeReview()
-                    } label: {
-                        Label("Write a Review", systemImage: "quote.bubble")
-                    }
-                    if MFMailComposeViewController.canSendMail() {
+            .overlay(alignment: .top) {
+                HStack {
+                    Menu {
                         Button {
-                            showEmailSheet.toggle()
+                            showShareSheet.toggle()
                         } label: {
-                            Label("Send us Feedback", systemImage: "envelope")
+                            Label("Share \(Constants.name)", systemImage: "square.and.arrow.up")
                         }
-                    } else if let url = Emails.mailtoUrl(subject: "\(Constants.name) Feedback"), UIApplication.shared.canOpenURL(url) {
                         Button {
-                            UIApplication.shared.open(url)
+                            Store.requestRating()
                         } label: {
-                            Label("Send us Feedback", systemImage: "envelope")
+                            Label("Rate \(Constants.name)", systemImage: "star")
                         }
+                        Button {
+                            Store.writeReview()
+                        } label: {
+                            Label("Write a Review", systemImage: "quote.bubble")
+                        }
+                        if MFMailComposeViewController.canSendMail() {
+                            Button {
+                                showEmailSheet.toggle()
+                            } label: {
+                                Label("Send us Feedback", systemImage: "envelope")
+                            }
+                        } else if let url = Emails.mailtoUrl(subject: "\(Constants.name) Feedback"), UIApplication.shared.canOpenURL(url) {
+                            Button {
+                                UIApplication.shared.open(url)
+                            } label: {
+                                Label("Send us Feedback", systemImage: "envelope")
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "info.circle")
                     }
-                } label: {
-                    Image(systemName: "info.circle")
+                    .font(.title)
+                    
+                    Spacer()
+                    Button("Reset") {
+                        count = 0
+                        Haptics.tap()
+                    }
+                    .font(.title2)
+                    
+                    Spacer()
+                    Menu {
+                        Button {} label: {
+                            Label("Tap with one finger to increment", systemImage: "circlebadge")
+                        }
+                        Button {} label: {
+                            Label("Tap with two fingers to decrement", systemImage: "circle.grid.2x1")
+                        }
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                    }
+                    .font(.title)
                 }
-                .font(.title)
                 .foregroundColor(.white)
                 .padding(.top)
-                .padding(.leading, 20)
-            }
-            .overlay(alignment: .topTrailing) {
-                Menu {
-                    Button {} label: {
-                        Label("Tap with one finger to increment", systemImage: "circlebadge")
-                    }
-                    Button {} label: {
-                        Label("Tap with two fingers to decrement", systemImage: "circle.grid.2x1")
-                    }
-                    Button {} label: {
-                        Label("Shake the device to reset", systemImage: "iphone.radiowaves.left.and.right")
-                    }
-                } label: {
-                    Image(systemName: "questionmark.circle")
-                }
-                .font(.title)
-                .foregroundColor(.white)
-                .padding(.top)
-                .padding(.trailing, 20)
-            }
-            .onShake {
-                if count != 0 {
-                    showResetAlert = true
-                    Haptics.error()
-                }
-            }
-            .alert("Reset Counter?", isPresented: $showResetAlert) {
-                Button("Cancel", role: .cancel) {}
-                Button("Reset", role: .destructive) {
-                    count = 0
-                    Haptics.tap()
-                }
+                .padding(.horizontal, 20)
             }
             .shareSheet(url: Constants.appUrl, showsSharedAlert: true, isPresented: $showShareSheet)
             .emailSheet(recipient: Constants.email, subject: "\(Constants.name) Feedback", isPresented: $showEmailSheet)
